f0c308b30645b1baf1df651fe6a8886b7fa716fa
CodingNija <hunterkile56@gmail.com>
2025-02-01 05:06:13 -0500

    completed index.js and created README

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# README_Generator
+
+## Description
+This is a command-line app using the Inquirer package to create a README.md.
+
+## Installation
+1. Clone the repository: `git clone https://github.com/username/repo.git` 
+2. Navigate to the project directory: `cd repo` 
+3. Install dependencies: `npm install`
+
+## Usage
+To use this project, run node index.js and follow the prompts.
+
+## Video Demo
+[![Watch the video]()]
+
+## License
+This project is licensed under the MIT license.
+
+## Questions
+If you have any questions, you can contact me at [user@example.com](mailto:user@example.com).
+You can also find more of my work at [CodeNinja-Hunter](https://github.com/CodeNinja-Hunter).
+  
--- index.js
+++ index.js
@@ -1,13 +1,83 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+import inquirer from 'inquirer';
+import fs from 'fs';
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What is the usage information?',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What license is the project under?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  }
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('README file created successfully!');
+    }
+  });
+}
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateReadme(answers);
+    writeToFile('README.md', readmeContent);
+  });
+}
+
+function generateReadme(answers) {
+  return `
+# ${answers.title}
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This project is licensed under the ${answers.license} license.
+
+## Questions
+If you have any questions, you can contact me at [${answers.email}](mailto:${answers.email}).
+You can also find more of my work at [${answers.github}](https://github.com/${answers.github}).
+  `;
+}
 
 // Function call to initialize app
 init();
